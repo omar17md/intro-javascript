81609c304ce5f56e7afc945c48c7c0cdcba65d04
Jesus Omar Magana Medina <jesus.magana@coppel.com>
2022-07-12 19:17:11 -0600

    Se realizo actividad 7 parte 3

---
+++ ACTVIDAD-7/Parte 3/Actividad 7-2.js
@@ -0,0 +1,14 @@
+// Ejercicio 7-1
+
+function fibonacci(tam, num1, num2){
+    if(tam==0)
+    {
+        return 1;
+    }
+    console.log(num1+num2 + ", ");
+    
+    return fibonacci(tam-1, num2, num2+num1);
+}
+
+console.log("1");
+fibonacci(6-1, 0, 1);
---
+++ ACTVIDAD-7/Parte 3/Actvidad 7-1.js
@@ -0,0 +1,21 @@
+// Ejercicio 7-1
+
+function multiply(a, b){
+    if(arguments.length == 2){
+        return a * b;
+    }else{
+        return multiply2;
+    }
+
+    function multiply2(c){
+        return c * a;
+    }
+}
+
+let fun = multiply(12, 2);
+console.log(fun);
+
+// let fun = multiply(12);
+// let res = fun(2);
+// console.log(res);
+
